cdaf796f648c6d5269bdac95ec21fa3510d9cf5c
Levi Adler <leviadler@gmail.com>
2014-05-23 14:33:13 -0400

    implement associatable part 1

--- lib/active_record_lite/04_associatable.rb
+++ lib/active_record_lite/04_associatable.rb
@@ -21,7 +21,7 @@ end
 class BelongsToOptions < AssocOptions
 
   def initialize(name, options = {})
-    default_fk = (name + "_id").to_sym
+    default_fk = (name.to_s + "_id").to_sym
     default_cn = name.to_s.camelcase
     defaults = {foreign_key: default_fk,
                 primary_key: :id,
@@ -36,7 +36,7 @@ end
 class HasManyOptions < AssocOptions
   def initialize(name, self_class_name, options = {})
     default_fk = (self_class_name.downcase.underscore + "_id").to_sym
-    default_cn = name.singularize.camelcase
+    default_cn = name.to_s.singularize.camelcase
     defaults = {foreign_key: default_fk,
                 primary_key: :id,
                 class_name: default_cn}
@@ -50,11 +50,22 @@ end
 module Associatable
   # Phase IVb
   def belongs_to(name, options = {})
-    # ...
+    options = BelongsToOptions.new(name, options)
+
+    define_method(name) do
+      foreign_key = self.send(options.foreign_key)
+      options.model_class.where(options.primary_key => foreign_key).first
+    end
+
   end
 
   def has_many(name, options = {})
-    # ...
+    options = HasManyOptions.new(name, self.to_s, options)
+
+    define_method(name) do
+      primary_key = self.send(options.primary_key)
+      options.model_class.where(options.foreign_key => primary_key)
+    end
   end
 
   def assoc_options
@@ -64,4 +75,5 @@ end
 
 class SQLObject
   # Mixin Associatable here...
+  extend Associatable
 end
